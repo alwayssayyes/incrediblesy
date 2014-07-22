6b1c135de93191444d307f74426d8b5c576bf8f9
soyoung <incrediblesy@gmail.com>
2014-07-22 17:09:14 +0900

    jquery 수정

--- app/static/js/test.js
+++ app/static/js/test.js
@@ -1,25 +1,28 @@
 
 $(document).ready(function(){
-	a('#one1','#two2','#three3','#four4');
+	a('#one1','#two2','#three3');
   $('a[href^="#"]').click(function(e){
     e.preventDefault();
     var target = this.hash;
-    $('.bigimg img').hide()
+    $('.album2 img').hide()
     $(target).fadeIn(1000)
   });
-  $('.imglist').fadeTo(0, 0.5)
-  $('.imglist').hover(
+
+
+  $('.album3').fadeTo(0, 0.5)
+  $('.album3').hover(
   	function(){
       $(this).fadeTo('fast',1)
-  	},
-  	function(){
+    },
+    function(){
       $(this).fadeTo('fast',0.5)
-  	});
+    });
+  // $('#one1').fadeOut
 })
 
 function a(v1, v2, v3, v4){
-   $(v1).fadeOut(1000)
-   $(v2).fadeIn(1000, function(){
-       a(v2,v3,v4,v1);
-   })
+ $(v1).fadeOut(1000)
+ $(v2).fadeIn(1000, function(){
+   a(v2,v3,v4,v1);
+ })
 }
